05a3a7a0e496690e1732c6c772c583f05f0991b4
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-01-31 23:50:58 +0100

    Update liste.blade.php

---
+++ resources/views/product/liste.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Product Project</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
+            <h1>Les Produits</h1>
+            <hr>
+            <a href="/ajouter" class="btn btn-primary">Ajouter un produit</a>
+            <hr>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nom du produit</th>
+                    <th>Prix</th>
+                    <th>Description</th>
+                    <th>Actions</th>
+                </tr>
+
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Telephone</td>
+                    <td>100 DH</td>
+                    <td>Nice product</td>
+                    <td>
+                        <a href="#" class="btn btn-primary">Modifier</a>
+                        <a href="#" class="btn btn-danger">Supprimer</a>
+
+                    </td>
+                </tr>
+                </tbody>
+
+            </table>
+        </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
