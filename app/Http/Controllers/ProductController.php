c918095dbb3217c35218d66736b2a450e2e4fa55
Yassine Bouaquil <139541292+yassine-bouaquil@users.noreply.github.com>
2024-01-31 23:50:41 +0100

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use  App\Models\Product;
+
 
 class ProductController extends Controller
 {
     public function liste_product(){
-        return view('product.list');
+        return view('product\liste');
+    }
+
+    public function ajouter_product(){
+        return view('product\ajouter');
+    }
+
+    public function Ajouter_product_traitement(Request $request){
+
+       $request->validate([
+           'nom'=>'required',
+            'prix'=>'required',
+            'description'=>'required',
+
+           ]
+       );
+       $product= new Product();
+       $product->nom = $request->nom;
+        $product->prix = $request->prix;
+        $product->description = $request->descriprtion;
+        $product->save();
+
+            return redirect(\ajouter)->with('status','L\Le produits a ete bin ajouter.');
+
     }
 }
